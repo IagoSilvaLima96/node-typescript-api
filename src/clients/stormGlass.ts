ec27ddb3cfebc6a2d9692cddb4e39b5820198a3f
Iago Silva Lima <iagocrouchlima@gmail.com>
2020-09-11 19:52:55 -0300

    Axios added, Storm glass client api and test created.

---
+++ src/clients/stormGlass.ts
@@ -0,0 +1,19 @@
+import { AxiosStatic } from 'axios';
+
+export class StormGlass {
+
+    readonly stormGlassAPIParams = 
+        'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+
+    readonly stormGlassAPISource = 'noaaa'
+
+    constructor(protected request: AxiosStatic) {
+
+    }
+
+    public async fetchPoints(lat: number, lng: number): Promise<{}> {
+        return this.request.get(
+            `http://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=${lat}&lng=${lng}`
+        );
+    }
+}
